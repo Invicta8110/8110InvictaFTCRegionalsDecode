0372a836a63f475916bf900c2c5c25178464c48d
Sparkle <rachmillie@yahoo.com>
2026-02-07 17:23:19 -0500

    Start of turret code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Turret.java
@@ -8,10 +8,18 @@ public class Turret {
     private Motor turretMotor;
 
     public Turret(HardwareMap hardwareMap, int goalTag) {
-
+        camera = new Camera(hardwareMap, goalTag);
+        outtakeMotor = new Motor("OttakeMotor",hardwareMap);
+        turretMotor = new Motor("TurretMotor",hardwareMap);
     }
 
     public void turnToGoal(double error) {
-
+        camera.poll();
+        if(!camera.tagFound()) {
+            turretMotor.setPower(.1);
+        }
+        else if(Math.abs(camera.getX())>error) {
+            turretMotor.setPower(camera.getX()*.03);
+        }
     }
 }
